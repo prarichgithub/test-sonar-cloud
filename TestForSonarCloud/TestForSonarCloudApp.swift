719d130d900faaaf9ca9f2af9deecac07e6d6711
prarichaya-as <prarichaya.as@tnis.com>
2022-02-28 13:05:00 +0700

    Initial Commit

---
+++ TestForSonarCloud/TestForSonarCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestForSonarCloudApp.swift
+//  TestForSonarCloud
+//
+//  Created by Prarichaya Assawanonwiwat on 28/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct TestForSonarCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
